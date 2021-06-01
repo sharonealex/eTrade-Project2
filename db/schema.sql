a371a8658ee8a70fc3e89ba3bb2d76d44004c30c
Sharon <sharonelizalex@gmail.com>
2021-06-01 13:04:43 +1000

    added initial table relationships

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,158 @@
 DROP DATABASE IF EXISTS eTrade_db;
 CREATE DATABASE eTrade_db;
+USE DATABASE eTrade_db;
+
+-- initial db tables design
+
+-- User Table
+
+
+CREATE TABLE user (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(45) NULL,
+  password_hash VARCHAR(50) NOT NULL,
+  first_name VARCHAR(45) NULL,
+  last_name DECIMAL(10,2) NULL,
+  phone_number INT NULL,
+  created_at DATETIME NULL DEFAULT NULL,
+  modified_at DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE user_address (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL
+    address_line1 VARCHAR(45) NULL,
+    address_line2 VARCHAR(45) NULL,
+    city VARCHAR(45) NULL,
+    postal_code VARCHAR(45) NULL,
+    country VARCHAR(45) NULL,
+    telephone VARCHAR(45) NULL,
+    mobile VARCHAR(45) NULL,
+)
+
+--one user can have multiple addresses.
+
+ALTER TABLE user_address ADD CONSTRAINT fk_user_address 
+    FOREIGN KEY (user_id) REFERENCES user(id);
+
+CREATE TABLE admin (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL,
+  password_hash VARCHAR(50) NOT NULL,
+  first_name VARCHAR(45) NULL,
+  last_name DECIMAL(10,2) NULL,
+  phone_number INT NULL,
+  permissions VARCHAR(50) NOT NULL,
+  created_at DATETIME NULL DEFAULT NULL,
+  modified_at DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+)    
+
+-- main product table
+
+CREATE TABLE product (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  description VARCHAR(50) NOT NULL,
+  SKU VARCHAR(45) NULL,
+  category_id INT NOT NULL
+  price VARCHAR(50) NOT NULL,
+  discount_id INT NOT NULL
+  created_at DATETIME NULL DEFAULT NULL,
+  modified_at DATETIME NULL DEFAULT NULL,
+  deleted_at DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+)  
+
+ALTER TABLE product ADD CONSTRAINT fk_category_id
+    FOREIGN KEY (category_id) REFERENCES product_category(id);
+ALTER TABLE product ADD CONSTRAINT fk_product_discount 
+    FOREIGN KEY (discount_id) REFERENCES product_discount(id);
+
+-- related tables 
+CREATE TABLE product_category (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  description VARCHAR(50) NOT NULL,
+  created_at DATETIME NULL DEFAULT NULL,
+  modified_at DATETIME NULL DEFAULT NULL,
+  deleted_at DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+) 
+
+CREATE TABLE product_discount (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  description VARCHAR(50) NOT NULL,
+  discount_percent DECIMAL(10,2),
+  active BOOLEAN,
+  created_at DATETIME NULL DEFAULT NULL,
+  modified_at DATETIME NULL DEFAULT NULL,
+  deleted_at DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+) 
+
+-- cart
+
+CREATE TABLE cart_item (
+  id INT NOT NULL AUTO_INCREMENT,
+  session_id INT NOT NULL,
+  product_id INT NOT NULL,
+  quantity INT NOT NULL,
+  created_at DATETIME NULL DEFAULT NULL,
+  modified_at DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+) 
+ALTER TABLE cart_item ADD CONSTRAINT fk_product_id
+    FOREIGN KEY (product_id) REFERENCES product(id);
+
+ALTER TABLE cart_item ADD CONSTRAINT fk_session_id
+    FOREIGN KEY (session_id) REFERENCES shopping_session(id);    
+
+
+-- shopping session id (session storage)
+
+CREATE TABLE shopping_session (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  created_at DATETIME NULL DEFAULT NULL,
+  modified_at DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+) 
+ALTER TABLE shopping_session ADD CONSTRAINT fk_user_id
+    FOREIGN KEY (user_id) REFERENCES user(id);
+
+-- many orders for one user
+-- user -> orders -> order_details for each order.
+
+CREATE TABLE order (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  order_id INT NOT NULL,
+  date_placed DATETIME NULL DEFAULT NULL,
+  amount INT NOT NULL,
+  created_at DATETIME NULL DEFAULT NULL,
+  modified_at DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+) 
+
+ALTER TABLE shopping_session ADD CONSTRAINT fk_user_id
+    FOREIGN KEY (user_id) REFERENCES user(id);
+
+ALTER TABLE shopping_session ADD CONSTRAINT fk_order_id
+    FOREIGN KEY (order_id) REFERENCES order_details(id);    
+
+
+CREATE TABLE order_details (
+  id INT NOT NULL AUTO_INCREMENT,
+  quantity INT NOT NULL
+  total INT NOT NULL,
+  product_id INT NOT NULL,
+  created_at DATETIME NULL DEFAULT NULL,
+  modified_at DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+) 
+
+ALTER TABLE order_details ADD CONSTRAINT fk_product_id
+    FOREIGN KEY (product_id) REFERENCES product(id);   
