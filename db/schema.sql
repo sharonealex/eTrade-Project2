cfb0c5b257c3e4ac6f62ae9169789c88341d0e4a
Sharon <sharonelizalex@gmail.com>
2021-06-02 20:01:43 +1000

    refactored schema relationships

--- db/schema.sql
+++ db/schema.sql
@@ -1,152 +1,105 @@
-DROP DATABASE IF EXISTS eTrade_db;
-CREATE DATABASE eTrade_db;
-USE  eTrade_db;
+drop database if exists etrade_db;
+create database etrade_db;
 
--- initial db tables design
 
--- User Table
+-- To be removed after models are created based off this schema
 
-CREATE TABLE user (
-  id INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(45) NULL,
-  password_hash VARCHAR(50) NOT NULL,
-  first_name VARCHAR(45) NULL,
-  last_name DECIMAL(10,2) NULL,
-  phone_number INT NULL,
-  created_at DATETIME NULL DEFAULT NULL,
-  modified_at DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (id)
-);
+CREATE TABLE IF NOT EXISTS `etrade_db`.`category_discount` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `duration_weeks` INT NOT NULL,
+  `discount_percent` DECIMAL(10,2) NULL DEFAULT NULL,
+  `active` TINYINT(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`));
 
-CREATE TABLE user_address (
-    id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    address_line1 VARCHAR(45) NULL,
-    address_line2 VARCHAR(45) NULL,
-    city VARCHAR(45) NULL,
-    postal_code VARCHAR(45) NULL,
-    country VARCHAR(45) NULL,
-    telephone VARCHAR(45) NULL,
-    mobile VARCHAR(45) NULL,
-     PRIMARY KEY (id)
-);
+CREATE TABLE IF NOT EXISTS `etrade_db`.`category` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `category_id` INT NOT NULL,
+  `name` VARCHAR(50) NOT NULL,
+  `description` VARCHAR(50) NOT NULL,
+  `discount_id` INT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_category_category1`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `etrade_db`.`category_discount` (`id`));
 
--- one user can have multiple addresses.
-
-CREATE TABLE admin (
-  id INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(50) NOT NULL,
-  password_hash VARCHAR(50) NOT NULL,
-  first_name VARCHAR(45) NULL,
-  last_name DECIMAL(10,2) NULL,
-  phone_number INT NULL,
-  permissions VARCHAR(50) NOT NULL,
-  created_at DATETIME NULL DEFAULT NULL,
-  modified_at DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE product_category (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL,
-  description VARCHAR(50) NOT NULL,
-  created_at DATETIME NULL DEFAULT NULL,
-  modified_at DATETIME NULL DEFAULT NULL,
-  deleted_at DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (id)
-) ;
-
-CREATE TABLE product_discount (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL,
-  description VARCHAR(50) NOT NULL,
-  discount_percent DECIMAL(10,2),
-  active BOOLEAN,
-  created_at DATETIME NULL DEFAULT NULL,
-  modified_at DATETIME NULL DEFAULT NULL,
-  deleted_at DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (id)
-) ;
--- main product table
-
-CREATE TABLE product (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL,
-  description VARCHAR(50) NOT NULL,
-  SKU VARCHAR(45) NULL,
-  category_id INT NOT NULL,
-  price VARCHAR(50) NOT NULL,
-  discount_id INT NOT NULL,
-  created_at DATETIME NULL DEFAULT NULL,
-  modified_at DATETIME NULL DEFAULT NULL,
-  deleted_at DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (id)
-);
-
--- related tables 
--- cart
-
-CREATE TABLE cart_item (
-  id INT NOT NULL AUTO_INCREMENT,
-  session_id INT NOT NULL,
-  product_id INT NOT NULL,
-  quantity INT NOT NULL,
-  created_at DATETIME NULL DEFAULT NULL,
-  modified_at DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (id)
-);
-
--- shopping session id (session storage)
-
-CREATE TABLE shopping_session (
-  id INT NOT NULL AUTO_INCREMENT,
-  user_id INT NOT NULL,
-  created_at DATETIME NULL DEFAULT NULL,
-  modified_at DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (id)
-);
-
--- many orders for one user
--- user -> orders -> order_details for each order.
-
-CREATE TABLE orders (
-  id INT NOT NULL AUTO_INCREMENT,
-  user_id INT NOT NULL,
-  order_id INT NOT NULL,
-  date_placed DATETIME NULL DEFAULT NULL,
-  amount INT NOT NULL,
-  created_at DATETIME NULL DEFAULT NULL,
-  modified_at DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE order_details (
-  id INT NOT NULL AUTO_INCREMENT,
-  quantity INT NOT NULL,
-  total INT NOT NULL,
-  product_id INT NOT NULL,
-  created_at DATETIME NULL DEFAULT NULL,
-  modified_at DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (id)
-) ;
-
-ALTER TABLE order_details ADD CONSTRAINT fk_product_id
-    FOREIGN KEY (product_id) REFERENCES product(id);   
+CREATE TABLE IF NOT EXISTS `etrade_db`.`product` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `description` VARCHAR(50) NOT NULL,
+  `category_id` INT NOT NULL,
+  `price` DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_category_id`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `etrade_db`.`category` (`id`));
+   
+CREATE TABLE IF NOT EXISTS `etrade_db`.`user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NULL DEFAULT NULL,
+  `password_hash` VARCHAR(50) NOT NULL,
+  `first_name` VARCHAR(45) NULL DEFAULT NULL,
+  `last_name` DECIMAL(10,2) NULL DEFAULT NULL,
+  `phone_number` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`));
+   
+CREATE TABLE IF NOT EXISTS `etrade_db`.`order` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `date_placed` DATETIME NULL DEFAULT NULL,
+  `total_price` INT NOT NULL,
+  `total_quantity` INT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_userid_order`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `etrade_db`.`user` (`id`));
     
-ALTER TABLE user_address ADD CONSTRAINT fk_user_id
-    FOREIGN KEY (user_id) REFERENCES user(id);
+CREATE TABLE IF NOT EXISTS `etrade_db`.`order_item` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `order_id` INT NULL,
+  `product_id` INT NOT NULL,
+  `product_quantity` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_product_id`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `etrade_db`.`product` (`id`),
+  CONSTRAINT `fk_order_id2`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `etrade_db`.`order` (`id`));
     
-ALTER TABLE product ADD CONSTRAINT fk_category_id
-    FOREIGN KEY (category_id) REFERENCES product_category(id);
+CREATE TABLE IF NOT EXISTS `etrade_db`.`address` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `address_line1` VARCHAR(45) NULL DEFAULT NULL,
+  `address_line2` VARCHAR(45) NULL DEFAULT NULL,
+  `city` VARCHAR(45) NULL DEFAULT NULL,
+  `postal_code` VARCHAR(45) NULL DEFAULT NULL,
+  `mobile` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `etrade_db`.`user` (`id`));
     
-ALTER TABLE product ADD CONSTRAINT fk_product_discount 
-    FOREIGN KEY (discount_id) REFERENCES product_discount(id);
+CREATE TABLE IF NOT EXISTS `etrade_db`.`cart` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `total_quantity` INT NOT NULL,
+  `total_price` DECIMAL(10,2) NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_cart_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `etrade_db`.`user` (`id`));
     
-ALTER TABLE shopping_session ADD CONSTRAINT fk_user_id2
-    FOREIGN KEY (user_id) REFERENCES user(id);
+CREATE TABLE IF NOT EXISTS `etrade_db`.`product_cart` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `product_id` INT NULL DEFAULT NULL,
+  `cart_id` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `prd_fk1`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `etrade_db`.`product` (`id`),
+  CONSTRAINT `crt_fk1`
+    FOREIGN KEY (`cart_id`)
+    REFERENCES `etrade_db`.`cart` (`id`));
+
     
-ALTER TABLE cart_item ADD CONSTRAINT fk_session_id
-    FOREIGN KEY (session_id) REFERENCES shopping_session(id);
-    
-ALTER TABLE cart_item ADD CONSTRAINT fk_product_id2
-    FOREIGN KEY (product_id) REFERENCES product(id);
