15457704ec4cb75f98eadcdb4418a848643fad4a
Sharon <sharonelizalex@gmail.com>
2021-06-05 14:00:49 +1000

    facing issue with sequelize

--- routes/index.js
+++ routes/index.js
@@ -1,63 +1,8 @@
-const router = require("express").Router();
-const userApi = require("./user");
+const router = require('express').Router();
+const apiRoutes = require('./api');
 
+router.use('/api', apiRoutes);
 
-// building out the nested routes
-router.use("/", userApi);
-//router.use('/api', apiRoutes);
-
-// common routes throughout the whole application
-router.get("/", () => {
-  res.render("all");
-});
-
-router.get("/about-us", (req, res) => {
-  res.render("about-us");
-});
-
-router.get("/contact-us", (req, res) => {
-  res.render("contact-us");
-});
-
-router.get("/legal", (req, res) => {
-  res.render("legal");
-});
-
-router.get("/customer-support", (req, res) => {
-  res.render("customer-support");
-});
-
-router.get("/track-order", (req, res) => {
-    res.render("track-order");
-});
-
-router.get('/mens', (req, res) => {
-  res.render("mens");
-});
-
-router.get('/womens', (req, res) => {
-  res.render('womens');
-});
-
-router.get('/kids', (req, res) => {
-  res.render("kids");
-});
-
-router.get("/login", (req, res) =>{
-    if(req.session.loggedIn){
-        res.redirect("/");
-        return;
-    }
-    res.render('login');
-});
-
-router.get("/register", (req, res) => {
-    res.render("register");
-});
-
-router.get("/clothing-details", (req, res) => {
-    res.render('clothing-details');
-});
 
 
 module.exports = router;
