c5cfcf020c1055cc07df16efde093429562ad7d6
Hayden Jones <haychjones@gmail.com>
2021-06-05 18:37:27 +1000

    Added comments to server.js

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
+// Dependencies
 const path = require("path");
 const express = require("express");
-// the module that was referenced in package.json was express-sessions
 const session = require("express-session");
-const exhbs = require("express-handlebars");
-// the routes we called ./container and they were holding an empty file
+const exphbs = require("express-handlebars");
 const routes = require("./routes");
 
-// TODO: CREATE THE PATH
-// please dont import modules or directories if they dont exist
-const helpers = require("./utils/helpers");
-
 const sequelize = require("./config/connection");
-// the session.store wasnt there
-// when you're copying code, please make sure that you copy it with the understanding of what it does
-// missing something like this isn't a fun task to debug
 const sequelizeStore = require("connect-session-sequelize")(session.Store);
 
+// Initialise and set port for server.
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// if using handlebars helpers, please define the helper...
-const hbs = exhbs.create({ helpers });
-
-const sess = {
+// Initialise session.
+const sessionInit = {
   secret: "Shh secret",
   cookie: {},
   resave: false,
@@ -31,18 +22,25 @@ const sess = {
     db: sequelize,
   }),
 };
+app.use(session(sessionInit));
 
-app.use(session(sess));
-
-app.engine("handlebars", hbs.engine);
+// Set handlebars as the engine for the app.
+app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
+// Initialise the parsing of json and string data through express.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// Identify routes for app.
 app.use(routes);
 
+// Turn on connection to database and then to the server.
+// Have force: set to false so that server keeps data when app is run.
+// Change to true if you want database reset to default.
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}/eTrade`));
+  app.listen(PORT, () =>
+    console.log(`Now listening on http://localhost:${PORT}/eTrade`)
+  );
 });
