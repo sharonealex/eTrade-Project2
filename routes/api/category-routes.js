8cb9be5782d652a187a0102713a97803408428a6
Sharon <sharonelizalex@gmail.com>
2021-06-06 18:15:37 +1000

    created routes for categories

---
+++ routes/api/category-routes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { Product, Category, Category_Discount } = require('../../models')
+
+//get Category
+
+router.get('/', async (req, res) => {
+    try {
+      const categories = await Category.findAll(
+        {
+          include: [{ model: Category_Discount }],
+        }
+      );
+      res.status(200).json(categories);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  //get Category By Id
+  
+  router.get('/:id', async (req, res) => {
+    try {
+      const category = await Category.findByPk(req.params.id, {
+        include: [{ model: Category_Discount }],
+      });
+  
+      if (!category) {
+        res.status(404).json({ message: 'No product found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(category);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  module.exports = router;
+
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const productRoutes = require('./product-routes');
+const categoryRoutes = require('./category-routes');
 
 router.use('/products', productRoutes);
+router.use('/categories', categoryRoutes);
 
 module.exports = router;
