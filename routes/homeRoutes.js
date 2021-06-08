acb6cba66729b4fcb5bfea115134bea9c8d36640
Sharon <sharonelizalex@gmail.com>
2021-06-08 17:55:26 +1000

    display mens category from database

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Product, Category } = require('../models')
 
 router.get('/', (req, res)=>{
    res.render('all');
@@ -24,8 +25,21 @@ router.get("/about-us", (req, res) => {
       res.render("track-order");
   });
 
-  router.get('/mens', (req, res) => {
-    res.render("mens");
+  router.get('/mens', async (req, res) => {
+    try {
+      const productsData = await Product.findAll(
+        { 
+          where: {
+            category_id: 2
+          },
+          include: [{ model: Category }]
+        }
+      );
+      const products = productsData.map((item) => item.get({ plain: true }));
+     res.render('mens', {products});
+    } catch (err) {
+      res.status(500).json(err);
+    }
   });
   
   router.get('/womens', (req, res) => {
