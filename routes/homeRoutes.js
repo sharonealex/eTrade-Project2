2ce602d920f2ed7bb190d7265e51ac4e703fc8ea
Hayden Jones <haychjones@gmail.com>
2021-06-08 21:06:55 +1000

    womens and kids clothing is now being added to the page from the db

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,74 +1,93 @@
-
-const router = require('express').Router();
-const { Product, Category } = require('../models')
+const router = require("express").Router();
+const { Product, Category } = require("../models");
 
 router.get("/", (req, res) => {
   res.render("all", { loggedIn: req.session.loggedIn });
 });
 
 router.get("/about-us", (req, res) => {
-    res.render("about-us");
-  });
+  res.render("about-us");
+});
 
-  router.get("/contact-us", (req, res) => {
-    res.render("contact-us");
-  });
+router.get("/contact-us", (req, res) => {
+  res.render("contact-us");
+});
 
-  router.get("/legal", (req, res) => {
-    res.render("legal");
-  });
-  
-  router.get("/customer-support", (req, res) => {
-    res.render("customer-support");
-  });
-  
-  router.get("/track-order", (req, res) => {
-      res.render("track-order");
-  });
+router.get("/legal", (req, res) => {
+  res.render("legal");
+});
 
-  router.get('/mens', async (req, res) => {
-    try {
-      const productsData = await Product.findAll(
-        { 
-          where: {
-            category_id: 2
-          },
-          include: [{ model: Category }]
-        }
-      );
-      const products = productsData.map((item) => item.get({ plain: true }));
-     res.render('mens', {products});
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+router.get("/customer-support", (req, res) => {
+  res.render("customer-support");
+});
 
-  router.get('/mens/:id', async (req, res) => {
-    try {
-      const productData = await Product.findByPk(req.params.id, {
-        include: [{ model: Category }],
-      });
-      const product = productData.get({ plain: true });
-     res.render('mens', {product});
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  
-  router.get('/womens', (req, res) => {
-    res.render('womens');
-  });
-  
-  router.get('/kids', (req, res) => {
-    res.render("kids");
-  });
+router.get("/track-order", (req, res) => {
+  res.render("track-order");
+});
 
-  router.get("/login", (req, res) =>{
-    // if(req.session.loggedIn){
-    //     res.redirect("/");
-    //     return;
-    // }
-    res.render('login');
+router.get("/mens", async (req, res) => {
+  try {
+    const productsData = await Product.findAll({
+      where: {
+        category_id: 2,
+      },
+      include: [{ model: Category }],
+    });
+    const products = productsData.map((item) => item.get({ plain: true }));
+    res.render("mens", { products });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/mens/:id", async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }],
+    });
+    const product = productData.get({ plain: true });
+    res.render("mens", { product });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/womens", async (req, res) => {
+  try {
+    const productsData = await Product.findAll({
+      where: {
+        category_id: 1,
+      },
+      include: [{ model: Category }],
+    });
+    const products = productsData.map((item) => item.get({ plain: true }));
+    res.render("womens", { products });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/kids", async (req, res) => {
+  try {
+    const productsData = await Product.findAll({
+      where: {
+        category_id: 3,
+      },
+      include: [{ model: Category }],
+    });
+    const products = productsData.map((item) => item.get({ plain: true }));
+    res.render("kids", { products });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // if(req.session.loggedIn){
+  //     res.redirect("/");
+  //     return;
+  // }
+  res.render("login");
 });
 router.get("/register", (req, res) => {
   res.render("register");
