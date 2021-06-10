4abb98609956674d40ce6064849c3d7b0455a22f
Sharon <sharonelizalex@gmail.com>
2021-06-10 13:32:57 +1000

    added backend for register user and address

--- routes/api/address-routes.js
+++ routes/api/address-routes.js
@@ -19,4 +19,30 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+
+router.post("/", async (req, res) => {
+  try {
+    const createAdress = await Address.create({
+      city: req.body.city,
+      address_line1: req.body.address_line1,
+      address_line2: req.body.address_line2,
+      postal_code: req.body.postal_code,
+      user_id: req.body.user_id
+    });
+
+    //console.log(createAdress);
+    const addressData = createAdress.get({ plain: true });
+
+    req.session.save(() => {
+      //   req.session.user_id = user.id
+      //   req.session.username = user.username;
+      req.session.loggedIn = true;
+      res.status(200).json(addressData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
